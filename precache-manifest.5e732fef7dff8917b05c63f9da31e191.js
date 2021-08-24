2a6f07c004ea755a64677ef9fd779981d9e36606
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2021-08-24 12:04:40 +0530

    Updates

---
+++ precache-manifest.5e732fef7dff8917b05c63f9da31e191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef5546ff9f707e636b5fc8441ac3727",
+    "url": "/Crypto-Tracker-React/index.html"
+  },
+  {
+    "revision": "da66bcf93646500ac82d",
+    "url": "/Crypto-Tracker-React/static/css/main.0c4a419c.chunk.css"
+  },
+  {
+    "revision": "221ccf573d17860662c2",
+    "url": "/Crypto-Tracker-React/static/js/2.a0cfb8b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Crypto-Tracker-React/static/js/2.a0cfb8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da66bcf93646500ac82d",
+    "url": "/Crypto-Tracker-React/static/js/main.8dd3357d.chunk.js"
+  },
+  {
+    "revision": "8b538c9a15b9d3cf577b",
+    "url": "/Crypto-Tracker-React/static/js/runtime-main.b3bbd93b.js"
+  }
+]);
